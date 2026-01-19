585acaac4a38e6827aae875dc537598ca2603929
Adam Ben Shoham <mada20942@gmail.com>
2026-01-19 13:26:32 +0200

    feat: import libraries, define sorting rules, find dowload folder path

---
+++ main.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+import shutil
+
+sorting_dict = {'Documents': ['.pdf', '.docx', '.txt', '.pptx', '.xlsx', '.csv'],
+                'Images': ['.png', '.jpg', '.jpeg', '.gif', '.svg', '.webp', '.tiff'],
+                'Videos': ['.mp4', '.mov', '.avi', '.mkv', '.wmv'],
+                'Software': ['.exe', '.msi', '.dmg', '.pkg'],
+                'Compressed': ['.zip', '.rar', '.7z', '.tar', '.gz'],
+                'Music': ['.mp3', '.m4a', '.flac', '.wav', '.aac'],
+                'Misc': []
+                }
+
+home = Path.home()
+download_folder = home / 'Downloads'
