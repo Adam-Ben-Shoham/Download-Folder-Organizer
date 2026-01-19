6930ec6992417b387a76afa9fb0f0731f25064ad
Adam Ben Shoham <mada20942@gmail.com>
2026-01-19 13:31:03 +0200

    feat: add sorting files functionality

--- main.py
+++ main.py
@@ -21,3 +21,21 @@ def check_for_folders():
         new_path.mkdir(parents=True, exist_ok=True)
 
 
+        if item.is_file() and not item.name.startswith('.') and item.name != 'main.py':
+
+            dest_folder = 'Misc'
+            for folder_name, extensions in sorting_dict.items():
+                if item.suffix.lower() in extensions:
+                    dest_folder = folder_name
+                    break
+
+            dir_path = download_folder / dest_folder / item.name
+
+            try:
+                shutil.move(item, dir_path)
+            except PermissionError:
+                print(f"Skipping {item.name}, Permission denied, File is open elsewhere...")
+            except FileExistsError:
+                print(f"File already exists in folder, renaming file to {item.stem}1{item.suffix}")
+            except OSError:
+                print(f"Could not move {item} to {dir_path}, OSError")
